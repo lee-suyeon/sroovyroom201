d5cd8219840bf74a162841467557e723779ff015
suyeon <sroovy@naver.com>
2020-11-14 23:22:37 +0900

    mongoDB 연결 URL 수정

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv:suyeon:dltndus1@sroovyroom201.vjb7e.mongodb.net/sroovyroom?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://suyeon:dltndus1@sroovyroom201.vjb7e.mongodb.net/sroovyroom?retryWrites=true&w=majority'
 }
