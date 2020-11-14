0ad0ffa1c0c860ed0a3c2ffdf3ca38e802d0275d
suyeon <sroovy@naver.com>
2020-11-14 22:59:54 +0900

    mongoose, bodyParser, config폴더 생성
    1. mongoose
    2. bodyParser - request data의 body로부터 파라미터 파싱
    3. mongoDB 연결 - prod, dev 분기처리

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3200
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+
+const { User } = require("./server/models/User");
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  useCreateIndex: true, 
+  useFindAndModify: false
+}).then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv:suyeon:dltndus1@sroovyroom201.vjb7e.mongodb.net/sroovyroom?retryWrites=true&w=majority'
+}
