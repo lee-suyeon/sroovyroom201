25e43a5a7137f502c0dabfc641b178ca57aa7de3
suyeon <sroovy@naver.com>
2020-11-15 15:27:16 +0900

    Bcrypt install
    비밀번호를 생성, 변경 할 때에만 비밀번호 암호화 하기

--- server/models/User.js
+++ server/models/User.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // salt length
 
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    maxlength: 10,
+    maxlength: 50
   },
   email: {
     type: String,
     trim: true,
+    unique: 1 
   },
   password: {
     type: String,
-    maxlength: 20,
+    maxlength: 50
   },
   role: { // host = 0, friend = 1, guest = 9
     type: Number,
@@ -22,7 +25,27 @@ const userSchema = mongoose.Schema({
     type: String
   },
   tokenExp: {
-    type: Number,
+    type: Number
+  }
+})
+
+// user모델에 user정보를 전달하기 전에 실행할 함수
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if(user.isModified('password')) { // 비밀번호를 변경 할 때만
+    // salt를 이용해 비밀번호 암호화
+    bcrypt.genSalt(saltRounds, function(err, salt) { 
+      if (err) return next(err)
+  
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) return next(err)
+        user.password = hash; // 해쉬된 비밀번호로 변경
+        next();
+      });
+    });
+  } else {
+    next();
   }
 })
 
