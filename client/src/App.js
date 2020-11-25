660b52ed2907566f69c40f589d48b705cf2a97e5
suyeon <sroovy@naver.com>
2020-11-25 23:12:45 +0900

    Landing Component 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import LandingPage from './components/views/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
+      <LandingPage />
     </div>
   );
 }
---
+++ client/src/components/views/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../../../utils/Logo';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <Logo />
+      <h1>
+          HELLO,
+      </h1>
+    </div>
+  )
+}
+
+export default LandingPage
