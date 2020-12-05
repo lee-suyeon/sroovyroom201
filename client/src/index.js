e4553ae43831dd4ac2896c984dc1137ef7793249
suyeon <sroovy@naver.com>
2020-12-05 12:40:27 +0900

    redux devtools, root reducer 생성

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import theme from './styles/theme';
 import GlobalStyle from './styles/global';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk) (createStore)
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-      <GlobalStyle />
-    </ThemeProvider>
+    <Provider store={createStoreWithMiddleware(Reducer, composeWithDevTools())}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <ThemeProvider theme={theme}>
+            <App />
+            <GlobalStyle />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
