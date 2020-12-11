c699cdb8307b9e10b5955fbd5ae24d281ec6fb49
suyeon <sroovy@naver.com>
2020-12-11 23:15:41 +0900

    Logo 사이즈별로 스타일 설정

--- client/src/utils/Logo.jsx
+++ client/src/utils/Logo.jsx
@@ -1,40 +1,115 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
+const sizes = {
+  large: {
+    width: '80px',
+    height: '80px',
+    fontSize: '1.6rem',
+    marginBottom: '1.5rem',
+  },
+  medium: {
+    width: '50px',
+    height: '50px',
+    fontSize: '1.25rem'
+  },
+  small: {
+    width: '30px',
+    height: '30px',
+    fontSize: '1rem'
+  },
+}
+
+const sizeStyle = css`
+  ${({ size }) => css`
+    width: ${sizes[size].width};
+    height: ${sizes[size].height};
+    font-size: ${sizes[size].fontSize};
+    margin-bottom: ${sizes[size].marginBottom};
+  `}
+`
 const MainLogo = styled.div`
   position: relative;
-  width: 50px;
-  height: 50px;
   border-radius: 50%;
-  background: ${props => props.color || '#04563f' }
-`
-
-const LogoText = styled.div`
-  position: absolute;
-  font-size: 1rem;
   font-weight: bold;
   color: #fff;
-  top: ${props => props.top};
-  left: ${props => props.left};
+  background-color: ${props => props.theme.mainColor};
+
+  /* size*/
+  ${sizeStyle}
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 50%; left: 50%;
+    transform: translate(-50%, -50%) rotate(45deg);
+    width: 1.5px;
+    height: 80%;
+    background: #fff;
+  }
+
+  span {
+    position: absolute;
+  }
+
+  .sroovy {
+    ${props =>
+      props.size === "large" &&
+      css`
+        top: 12px;
+        left: 20px;
+      `}
+    ${props =>
+      props.size === "medium" &&
+      css`
+        top: 0;
+        left: 0;
+      `}
+    ${props =>
+      props.size === "small" &&
+      css`
+        top: 0;
+        left: 0;
+      `}
+    }
+  }
+
+  .room {
+    ${props =>
+      props.size === "large" &&
+      css`
+        right: 20px;
+        bottom: 12px;
+      `}
+    ${props =>
+      props.size === "medium" &&
+      css`
+        top: 0;
+        left: 0;
+      `}
+    ${props =>
+      props.size === "small" &&
+      css`
+        top: 0;
+        left: 0;
+      `}
+    }
+  }
 `
 
-const Bar = styled.div`
-  position: absolute;
-  top: 50%; left: 50%;
-  transform: translate(-50%, -50%) rotate(45deg);
-  width: 2px;
-  height: 35px;
-  background: #fff;
-`
 
-function Logo() {
+
+function Logo({ size, ...rest }) {
   return (
-    <MainLogo>
-      <LogoText top="6px" left="13px">S</LogoText>
-      <Bar />
-      <LogoText top="25px" left="30px">R</LogoText>
+    <MainLogo size={size} >
+      <span className="sroovy" >S</span>
+      <span className="room">R</span>
     </MainLogo>
   )
 }
+
+Logo.defaultProps = {
+  size: 'medium'
+};
 
 export default Logo
