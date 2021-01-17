1d4d9b7f362934bb0cc893ae235cbe0521725259
suyeon <sroovy@naver.com>
2021-01-17 13:01:33 +0900

    로그인페이지 디자인 수정

--- client/src/components/views/LoginPage/LoginPage.jsx
+++ client/src/components/views/LoginPage/LoginPage.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
+
+import moment from 'moment';
+
 import { loginUser } from '../../../_actions/user_action';
 
 import styled from 'styled-components';
 import Button from '../../../utils/Button';
 import TextInput from '../../../utils/TextInput';
 import Logo from '../../../utils/Logo';
+import TextLogo from '../../../utils/TextLogo';
+import Bar from '../../../utils/Bar';
 import { Title, Text } from '../../../utils/Typo';
+
+import { PhoneCall, UserPlus } from 'react-feather';
 
 const ContentPage = styled.div`
   display: flex;
@@ -18,11 +25,35 @@ const ContentPage = styled.div`
   padding-bottom: 2.5rem;
 `
 
+const DayCounter = styled.div`
+  text-align: right;
+  color: ${({ theme }) => theme.textColor };
+
+  .day-counter {
+    color:  ${({ theme }) => theme.mainColor };
+  }
+`
+
+const IconButton = styled.div`
+  flex: 1 1;
+  color: ${({ theme }) => theme.mainColor };
+  text-align: center;
+
+  span {
+    display: block;
+    line-height: 1.5;
+    font-size: 0.85rem;
+    margin-top: 0.5rem;
+  }
+`
+
 function LoginPage(props) {
 
   const dispatch = useDispatch();
   const [ email, setEmail ] = useState("");
   const [ password, setPassword ] = useState("");
+
+  const dayCount = moment().diff(moment('2020-10-18'), "days");
 
   const onEmailHandler = (e) => {
     setEmail(e.currentTarget.value);
@@ -51,32 +82,56 @@ function LoginPage(props) {
 
   return (
     <ContentPage>
-      <Logo size="large" />
-      <Text margin={"0 0 0.5rem"}>WELCOME BACK!</Text>
-      <Title>ENTERING THE SROOVYROOM201</Title>
+      <div className="top-content" style={{ padding: '1rem'}}>
+        <Logo size="large" />
+        <TextLogo size="large" />
+        <Title>체크인하기</Title>
+        <DayCounter>
+          독립 
+          <span className="day-counter"> {dayCount}</span>
+          일 째
+        </DayCounter>
+      </div>
+
+      <Bar/>
 
       <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
         <TextInput 
           type="email"
-          placeholder="e-mail"
+          label="E-mail"
+          placeholder="이메일을 입력해주세요"
           value={email}
           onChange={onEmailHandler}
         />
-        <TextInput 
+        <TextInput
           type="password"
-          placeholder="password"
+          label="Password"
+          placeholder="비밀번호를 입력해주세요"
           value={password}
           onChange={onPasswordHandler}
         />
-        <Button>ENTER</Button>
+        <div style={{ display: 'flex', width: '100%', marginBottom: '2rem' }}>
+          <IconButton>
+            <a href="tel:01099386438">
+              <PhoneCall />
+              <span>
+                아무것도 기억이 안나요. <br />
+                호스트에게 전화 걸기
+              </span>
+            </a>
+          </IconButton>
+          <IconButton>
+            <Link to="/register">
+              <UserPlus />
+              <span>
+                아직 친구가 아니에요. <br />
+                친구하러 가기
+              </span>
+            </Link>
+          </IconButton>
+        </div>
+        <Button width="full" size="medium">ENTER</Button>
       </form>
-      <Text 
-        textAlign={"center"}
-        textDecoration={"underline"}
-        margin={"2rem 0 0 0"}
-      >
-        BE FRIEND WITH SROOVYROOM
-      </Text>
     </ContentPage>
   )
 }
