3cde22d4e055f97628d44086ed77f1e6981bab28
suyeon <sroovy@naver.com>
2021-01-31 12:45:30 +0900

    [feat] : Add color props and Set default props

--- client/src/utils/TextLogo.jsx
+++ client/src/utils/TextLogo.jsx
@@ -18,22 +18,35 @@ const sizeStyle = css`
     font-size: ${sizes[size].fontSize};
   `}
 `
-
 const SroovyRoom = styled.div`
+  display: inline-block;
   font-family: 'Dosis', sans-serif;
   font-weight: 600;
-  color: #779482;
+  color: ${({ theme, color }) => color || theme.mainColor };
   margin-bottom: 0.2rem;
+
+  ${props =>
+    props.color === "text" &&
+    css`
+      color: #333;
+    `}
 
   ${sizeStyle}
 `
 
-function TextLogo({ size, ...rest }) {
+function TextLogo({ size, color, ...rest }) {
   return (
-    <div>
-      <SroovyRoom size={size} {...rest}>SROOVYROOM</SroovyRoom>
-    </div>
+    <SroovyRoom 
+      size={size} 
+      color={color} 
+      {...rest}
+    >SROOVYROOM</SroovyRoom>
   )
 }
+
+TextLogo.defaultProps = {
+  TextLogo: 'textColor',
+  size: 'large'
+};
 
 export default TextLogo
