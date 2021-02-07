1a648d5d72d74259c209ebb21cf3aa2bb34cf162
suyeon <sroovy@naver.com>
2021-02-07 13:27:32 +0900

    [feat] Add SelectBox common UI

---
+++ client/src/utils/SelectBox.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledSelectBox = styled.div`
+  margin-bottom: 2rem;
+
+  label {
+    display: block;
+    color: ${({ theme }) => theme.mainColor };
+    font-weight: 500;
+    margin-bottom: 1.2rem;
+  }
+
+  select {
+    width: 100%;
+    height: 40px;
+    border: 1px solid ${({ theme }) => theme.mainColor };
+    padding: 0.5rem;
+    appearance: none;
+  }
+
+  select::placeholder {
+    font-size: 0.825rem;
+  }
+
+  .dropdown { position: relative; }
+
+  .dropdown:after {
+    content: '';
+    position: absolute;
+    top: 50%; right: 0.7rem;
+    transform: translateY(-50%);
+    width: 0px;
+    height: 0px;
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+    border-top: 10px solid ${({ theme }) => theme.mainColor };
+  }
+`
+
+
+const Selectbox = ({
+  datas,
+  id,
+  onChange,
+  disabled,
+  value,
+  showPlaceholder,
+  placeholder,
+  label
+}) => {
+  const dataList = datas.map((data) => {
+    return (
+      <option 
+        key={data.idx} 
+        value={data.idx}
+      >
+        {data.desc ? `${data.name} - ${data.desc}` : data.name}
+      </option>
+    );
+  });
+
+  return (
+    <StyledSelectBox>
+      <label>{label}</label>
+      <div className="dropdown">
+        <select
+          className="selectbox"
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+        >
+          {showPlaceholder && (
+            <option value={-1} hidden>
+              {placeholder}
+            </option>
+          )}
+          {dataList}
+        </select>
+      </div>
+    </StyledSelectBox>
+  );
+};
+
+export default Selectbox;
