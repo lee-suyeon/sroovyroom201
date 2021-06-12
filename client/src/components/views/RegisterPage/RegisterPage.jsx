47d710ff472fb2eea5a9bf9a7678db4f9b7e0bbc
suyeon <sroovy@naver.com>
2021-06-12 12:11:40 +0900

    [style] Separate mbtiList and Change Style

--- client/src/components/views/RegisterPage/RegisterPage.jsx
+++ client/src/components/views/RegisterPage/RegisterPage.jsx
@@ -1,36 +1,19 @@
 import React, { useState } from 'react'
 import { withRouter } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { registerUser } from '../../../_actions/user_action';
+import { registerUser } from '_actions/user_action';
 
 import styled from 'styled-components';
-import Button from '../../../utils/Button';
-import TextInput from '../../../utils/TextInput';
-import Logo from '../../../utils/Logo';
-import TextLogo from '../../../utils/TextLogo';
-import Bar from '../../../utils/Bar';
-import SelectBox from '../../../utils/SelectBox';
-import { Title, Text } from '../../../utils/Typo';
+import Button from 'utils/Button';
+import TextInput from 'utils/TextInput';
+import Logo from 'utils/Logo';
+import TextLogo from 'utils/TextLogo';
+import Bar from 'utils/Bar';
+import SelectBox from 'utils/SelectBox';
+import PageContent from 'utils/PageContent';
+import { Title, Text } from 'utils/Typo';
 
-const mbtiList = [
-  { idx: 0, name: "모르겠어요.", desc: "" },
-  { idx: 1, name: "ISTJ", desc: "청렴결백 논리주의자" },
-  { idx: 2, name: "ISFJ", desc: "용감한 수호자" },
-  { idx: 3, name: "INFJ", desc: "선의의 옹호자" },
-  { idx: 4, name: "INTJ", desc: "용의주도한 전략가" },
-  { idx: 5, name: "ISTP", desc: "만능 재주꾼" },
-  { idx: 6, name: "ISFP", desc: "호기심 많은 예술가" },
-  { idx: 7, name: "INFP", desc: "열정적인 중재자" },
-  { idx: 8, name: "INTP", desc: "논리적인 사색가" },
-  { idx: 9, name: "ESTJ", desc: "엄격한 관리자" },
-  { idx: 10, name: "ESFJ", desc: "사교적인 외교관" },
-  { idx: 11, name: "ENFJ", desc: "정의로운 사회운동가" },
-  { idx: 12, name: "ENTJ", desc: "대담한 통솔자" },
-  { idx: 13, name: "ESTP", desc: "모험을 즐기는 사업가" },
-  { idx: 14, name: "ESFP", desc: "자유로운 영혼의 연예인" },
-  { idx: 15, name: "ENFP", desc: "재기발랄한 활동가" },
-  { idx: 16, name: "ENTP", desc: "논쟁을 즐기는 변론가" },
-]
+import mbtiList from './mbti'
 
 const ContentPage = styled.div`
   display: flex;
@@ -38,17 +21,18 @@ const ContentPage = styled.div`
   justify-content: center;
   width: 100%;
   height: 100%;
-  padding-bottom: 2.5rem;
-
-  form {
-    display: flex;
-    flex-direction: column;
-    padding: 0 0.5rem;
-  }
+  padding: 2rem;
 `
 
 const FreeDrink = styled.div`
   font-size: 0.8rem;
+`
+
+const InputForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+  padding: 0 1rem 3rem;
 `
 
 function RegisterPage(props) {
@@ -98,20 +82,23 @@ function RegisterPage(props) {
       });
   }
 
+  const freeDrink = (
+    <FreeDrink>
+      <TextLogo size="small" color="text" />과 친구하면 웰컴 드링크🍹를 드려요!
+    </FreeDrink>
+  )
+
   return (
     <ContentPage>
-      <div className="top-content" style={{ padding: '1rem 0.5rem'}}>
-        <Logo size="large" />
-        <TextLogo size="large" />
-        <Title>과 친구되기 </Title>
-        <FreeDrink>
-          <TextLogo size="small" color="text" />과 친구가 되면 웰컴 드링크🍹 한잔을 <br />무료로 드립니다. 
-        </FreeDrink>
-      </div>
+      <PageContent
+        title="과 친구되기"
+        desc={freeDrink}
+      >
 
+      </PageContent>
       <Bar/>
 
-      <form onSubmit={onSubmitHandler}>
+      <InputForm onSubmit={onSubmitHandler}>
         <TextInput
           label="E-mail"
           type="email"
@@ -141,19 +128,19 @@ function RegisterPage(props) {
           datas={mbtiList}
           value={mbti}
           onChange={onChangeSelect}
-          placeholder="당신의 MBTI는?"
+          placeholder="MBTI 알려주시겠어요?"
           showPlaceholder={true}
         />
         <TextInput
           label={`To SROOVYROOM`}
           name="message"
           type="text"
-          placeholder="하고싶은 말이 있으면 적어주세요. 없으면 서운해요."
+          placeholder="하고싶은 말을 적어주세요. 없으면 서운해요.😢"
           value={message}
           onChange={onChangeInput}
         />
-        <Button fullWidth size="medium">친구맺기</Button>
-      </form>
+        <Button fullWidth size="medium">FRIEND</Button>
+      </InputForm>
     </ContentPage>
   )
 }
---
+++ client/src/components/views/RegisterPage/mbti.js
@@ -0,0 +1,21 @@
+const mbtiList = [
+  { idx: 0, name: "모르겠어요.", desc: "" },
+  { idx: 1, name: "ISTJ", desc: "청렴결백 논리주의자" },
+  { idx: 2, name: "ISFJ", desc: "용감한 수호자" },
+  { idx: 3, name: "INFJ", desc: "선의의 옹호자" },
+  { idx: 4, name: "INTJ", desc: "용의주도한 전략가" },
+  { idx: 5, name: "ISTP", desc: "만능 재주꾼" },
+  { idx: 6, name: "ISFP", desc: "호기심 많은 예술가" },
+  { idx: 7, name: "INFP", desc: "열정적인 중재자" },
+  { idx: 8, name: "INTP", desc: "논리적인 사색가" },
+  { idx: 9, name: "ESTJ", desc: "엄격한 관리자" },
+  { idx: 10, name: "ESFJ", desc: "사교적인 외교관" },
+  { idx: 11, name: "ENFJ", desc: "정의로운 사회운동가" },
+  { idx: 12, name: "ENTJ", desc: "대담한 통솔자" },
+  { idx: 13, name: "ESTP", desc: "모험을 즐기는 사업가" },
+  { idx: 14, name: "ESFP", desc: "자유로운 영혼의 연예인" },
+  { idx: 15, name: "ENFP", desc: "재기발랄한 활동가" },
+  { idx: 16, name: "ENTP", desc: "논쟁을 즐기는 변론가" },
+]
+
+export default mbtiList;
