669fe9bbe9e268b72dd4b0678afaec473279f097
leesuyeon <42996064+lee-suyeon@users.noreply.github.com>
2021-08-26 23:31:24 +0900

    Delete config.js

--- client/src/config.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'https://api.instagram.com/'
-export const API_TOKEN = 'IGQVJYRTJCVm5ZAa3dTNTFDY25mdGFqNlh1cUVTU0JXX29rQ1pDTGswZAThDbUlvNlZABSUxkcWRtejNCV2tXcWVuN19wRC1CNWVENExNSXpxdXdzbVFqOWNLa1M5ZADV4TnNEMGdrYjlVNktJaHRaWWN2aAZDZD'
-export const MEDIA_BASE_URL = 'https://graph.instagram.com/'
