4a5a170a6ac8eb7d7432d411bc43dc7299c48291
suyeon <sroovy@naver.com>
2021-08-28 23:04:05 +0900

    [feat] Change the query to return the result after saving the visitors
    * 저장된 결과를 반환하도록 api 수정

--- server/routes/visitors.js
+++ server/routes/visitors.js
@@ -1,18 +1,24 @@
+const { json } = require('body-parser');
 const express = require('express');
 const router = express.Router();
 
 const { Visitors } = require('../models/Visitors');
 
 // 방명록 작성
-router.post('/', (req, res) => {
+router.post('/saveVisitors', (req, res) => {
 
   const visitors = new Visitors(req.body)
 
   visitors.save(err => {
     if(err) return res.status(400).json({ success: false, err })
-    return res.status(200).json({ success: true })
+    
+    Visitors.find({ '_id': visitors._id })
+      .populate('writer') // 모든 writer의 정보를 가져옴
+      .exec( (err, result) => {
+        if(err) return res.json({ success: false, err })
+        res.status(200).json({ success: true, result })
+      })
   })
-  
 });
 
 // 방명록 불러오기
