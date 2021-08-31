9f14fc7c14664c44e64147cd6cf2ad65f5a424c1
suyeon <sroovy@naver.com>
2021-08-31 22:54:24 +0900

    [fix] Edit refreshing page error
    * 새로고침 했을 때 Internal Server Error 문제

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const path = require("path");
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
