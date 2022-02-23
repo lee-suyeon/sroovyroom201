fa951f4cf4125d6acc4b4029dc47bac778e8e14e
suyeon <sroovy@naver.com>
2022-02-23 12:39:36 +0900

    [feat] Add send mail router
    * nodemailer로 예약 확인 메일 보내는 api 생성

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI
+  mongoURI: process.env.MONGO_URI,
+  nodemailerUser: process.env.NODEMAILER_USER,
+  nodemailerPass: process.env.NODEMAILER_PASS,
 }
--- server/routes/booking.js
+++ server/routes/booking.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
+const config = require('../config/key');
 
 const { Booking } = require('../models/Booking');
 
@@ -13,5 +15,44 @@ router.post('/', (req, res) => {
   })
   
 });
+
+router.post('/booking', (req, res) => {
+  
+  let data = req.body;
+  let transporter = nodemailer.createTransport({
+    service: 'Naver',
+    host: 'smtp.naver.com',
+    port: 465,
+    secure: false,
+    auth: {
+      user: config.nodemailerUser,
+      pass: config.nodemailerPass,
+    },
+  })
+
+  let mailOptions = {
+    from: `SROOVYROOM201 <${config.nodemailerUser}>`,
+    to: data.email ,
+    subject: 'SROOVYROOM201 예약확인메일입니다.',
+    text: data.booker,
+    html: `<h3>예약 확인 메일입니다.</h3>
+      <ul>
+        <li>날짜 : <strong>${data.checkIn} ~ ${data.checkOut} (${data.nights + 1}일)</strong></li>
+        <li>방문시간 : <strong>${data.visitTime}</strong></li>
+        <li>인원수 : <strong>${data.headCount}명</strong></li>
+        <li>메뉴 : <strong>${data.dinnerMenu}</strong></li>
+      </ul>
+      <br />
+      <p>스루비룸에서 만나요~~~</p>
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+  });
+})
 
 module.exports = router;
